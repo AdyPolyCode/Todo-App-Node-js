4c999c5630f794294c67cad1cb908a1eb0c14974
Ady <polcsek.dm90@gmail.com>
2021-10-09 19:45:15 -0700

    style: add minor style change

--- server.js
+++ server.js
@@ -1,37 +1,38 @@
-const colors = require('colors');
-const fastify = require('fastify')({ logger: { prettyPrint: true } });
-const dotenv = require('dotenv').config({ path: '.env' });
-
-
+require("colors");
+require("dotenv").config({ path: ".env" });
+const fastify = require("fastify")({ logger: { prettyPrint: true } });
 
 // register route plugins
-fastify.register(require('./src/db/dbConnect'));
-fastify.register(require('./src/routes/todos'), { prefix: '/api/todos' });
-
-
+fastify.register(require("./src/db/dbConnect"));
+fastify.register(require("./src/routes/todos"), { prefix: "/api/todos" });
 
 // register error handler instead of default
-fastify.setErrorHandler(require('./src/middlewares/errorHandler'));
-
-
+fastify.setErrorHandler(require("./src/middlewares/errorHandler"));
 
 // server listening for requests
 const server = async () => {
     try {
-        await fastify.ready(err => err ? console.log(err) : console.log('All plugins loaded successfully'.bgWhite.black.bold))
-        await fastify.listen(process.env.FASTY_PORT)
+        await fastify.ready((err) => {
+            if (err) {
+                console.log(err);
+            }
+            if (!err) {
+                console.log(
+                    "All plugins loaded successfully".bgWhite.black.bold
+                );
+            }
+        });
+        await fastify.listen(process.env.FASTY_PORT);
     } catch (error) {
-        fastify.log.error(error.red.underline.bold)
-        process.exit(1)
+        fastify.log.error(error.red.underline.bold);
+        process.exit(1);
     }
 };
 
-
 server();
 
-
 // handle error in promises
-process.on('unhandledRejection', function(reason, promise){
-    console.log('Unhandled Rejection: ', reason.stack)
-    process.exit(1)
+process.on("unhandledRejection", (reason, promise) => {
+    console.log("Unhandled Rejection: ", reason.stack);
+    process.exit(1);
 });
