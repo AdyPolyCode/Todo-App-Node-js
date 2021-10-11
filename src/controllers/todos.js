6d937b8088a4bda222ee296e6febe16a82f26aeb
Ady <polcsek.dm90@gmail.com>
2021-10-10 20:10:26 -0700

    chore: fix swap + create + update

--- src/controllers/todos.js
+++ src/controllers/todos.js
@@ -1,83 +1,91 @@
-const Todos = require('../models/Todos');
-const CustomError = require('../utils/customError');
+const Todos = require("../models/Todos");
+const CustomError = require("../utils/customError");
 
-
-exports.getTodos = async function(req, res){
-    let todos = await Todos.find({})
+exports.getTodos = async function (req, res) {
+    let todos = await Todos.find({});
 
     res.code(200).send({
         data: todos,
-        message: 'Successfully fetched',
-        count: todos.length
-    })
+        message: "Successfully fetched",
+        count: todos.length,
+    });
 };
 
+exports.getTodo = async function (req, res) {
+    const todo = await Todos.findById(req.params.todoId);
 
-exports.getTodo = async function(req, res){
-    const todo = await Todos.findById(req.params.todoId)
-
-    if(!todo){
-        return new CustomError('Item Not Found', 404)
+    if (!todo) {
+        return new CustomError("Item Not Found", 404);
     }
 
     res.code(200).send({
         data: todo,
-        message: 'Successfully fetched'
-    })
+        message: "Successfully fetched",
+    });
 };
 
-
-exports.createTodo = async function(req, res){
-    const todo = await Todos.create(req.body)
+exports.createTodo = async function (req, res) {
+    const todo = await Todos.create(req.body);
 
     res.code(201).send({
         data: todo,
-        message: 'Todo successfully created'
-    })
+        message: "Todo successfully created",
+    });
 };
 
+exports.updateTodo = async function (req, res) {
+    const todo = await Todos.findByIdAndUpdate(req.params.todoId, req.body, {
+        new: true,
+    });
 
-exports.updateTodo = async function(req, res){
-    const todo = await Todos.findByIdAndUpdate(req.params.todoId, req.body, { new: true })
-
-    if(!todo){
-        return new CustomError('Todo Not Found', 404)
+    if (!todo) {
+        return new CustomError("Todo Not Found", 404);
     }
 
     res.code(200).send({
         data: todo,
-        message: 'Todo successfully updated'
-    })
+        message: "Todo successfully updated",
+    });
 };
 
+exports.swapTodos = async function (req, res) {
+    let firstTodo;
+    let secondTodo;
 
-exports.updateTodos = async function(req, res){
-    const firstData = req.body[0]
-    const secondData = req.body[1]
+    [firstTodo, secondTodo] = [...req.body];
 
-    const firstTodo = await Todos.updateOnSwap(firstData._id, secondData.taskPosition)
+    [firstTodo.taskPosition, secondTodo.taskPosition] = [
+        secondTodo.taskPosition,
+        firstTodo.taskPosition,
+    ];
 
-    const secondTodo = await Todos.updateOnSwap(secondData._id, firstData.taskPosition)
+    firstTodo = await Todos.findByIdAndUpdate(
+        { _id: firstTodo._id },
+        { taskPosition: firstTodo.taskPosition }
+    );
+    secondTodo = await Todos.findByIdAndUpdate(
+        { _id: secondTodo._id },
+        { taskPosition: secondTodo.taskPosition }
+    );
 
-    if(!firstTodo || !secondTodo){
-        return new CustomError('Todo Not Found', 404)
+    if (!firstTodo || !secondTodo) {
+        return new CustomError("Todo Not Found", 404);
     }
 
     res.code(200).send({
-        message: 'Todos successfully updated'
-    })
+        message: "Todos successfully updated",
+    });
 };
 
+exports.deleteTodo = async function (req, res) {
+    const todo = await Todos.findByIdAndDelete(req.params.todoId);
 
-exports.deleteTodo = async function(req, res){
-    const todo = await Todos.findByIdAndDelete(req.params.todoId)
-
-    if(!todo){
-        return new CustomError('Todo Not Found', 404)
+    if (!todo) {
+        return new CustomError("Todo Not Found", 404);
     }
 
     res.code(200).send({
         data: todo,
-        message: 'Todo successfully deleted'
-    })
+        message: "Todo successfully deleted",
+    });
 };
