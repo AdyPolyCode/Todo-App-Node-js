6aa08fdcba95ca8a3a5c7c59e6f90705fc5aead3
Ady <polcsek.dm90@gmail.com>
2021-10-10 20:20:38 -0700

    chore: add dev log

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,24 +1,27 @@
-const CustomError = require('../utils/customError');
+const CustomError = require("../utils/customError");
 
+function errorHandler(err, req, res) {
+    let error = { ...err };
+    error.message = err.message;
 
-function errorHandler(err, req, res){
-    let error = {...err}
-    error.message = err.message
+    // log error in development
+    if (process.env.FASTY_ENV === "dev") {
+        console.log(err);
+    }
 
     // invalid object id for mongoose
-    if(err.name === 'CastError'){
-        error = new CustomError('Invalid Object Id', 400)
+    if (err.name === "CastError") {
+        error = new CustomError("Invalid Object Id", 400);
     }
 
     // cannot create duplicate items
-    if(err.code === 11000){
-        error = new CustomError('Todo already exists', 400)
+    if (err.code === 11000) {
+        error = new CustomError("Todo already exists", 400);
     }
 
     res.code(error.status || 500).send({
-        message: error.message || 'Internal Server Error'
-    })
-};
-
+        message: error.message || "Internal Server Error",
+    });
+}
 
 module.exports = errorHandler;
