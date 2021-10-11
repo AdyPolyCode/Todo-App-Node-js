b5b0bb25f4c29dbd5d9186880e797408147c95f8
Ady <polcsek.dm90@gmail.com>
2021-10-10 20:17:51 -0700

    style: format code

--- src/db/dbConnect.js
+++ src/db/dbConnect.js
@@ -1,15 +1,14 @@
-const fp = require('fastify-plugin');
-const mongoose = require('mongoose');
+const fp = require("fastify-plugin");
+const mongoose = require("mongoose");
 
-
-async function dbConnect(){
+async function dbConnect() {
     try {
-        const connect = await mongoose.connect(process.env.MONGO_URI)
-        console.log(`Successfully connected to ${connect.connection.host}`)
+        const connect = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Successfully connected to ${connect.connection.host}`);
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        console.log(error);
+        process.exit(1);
     }
-};
+}
 
 module.exports = fp(dbConnect);
